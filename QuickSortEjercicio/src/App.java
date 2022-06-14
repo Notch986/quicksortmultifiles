01b95282b3dae39d9e221fc018d9aef290fdd5e7
PanoccaJhon <jpanoccam@unsa.edu.pe>
2022-06-14 16:51:37 -0500

    documentado de codigo

--- QuickSortEjercicio/src/App.java
+++ QuickSortEjercicio/src/App.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import javax.swing.JFileChooser;
 
 import FileReader.CreateFile;
+
 import FileReader.ReadFile;
 import Sort.QuickSort;
 public class App {
@@ -21,27 +22,33 @@ public class App {
 		 	File[] files = chooser.getSelectedFiles();
 			
 		 	ArrayList<String> valores = new ArrayList<String>();
+			//Creamos el arreglo donde guardaremos los números de los archivos txt
 
 			ReadFile[] archivos = new ReadFile[files.length];
-				
+
+			//Iniciamos un bucle con el número de archivos seleccionados y trabajamos con el arreglo creado
+			//que contiene los números de cada archivo txt	
 			for (int i = 0; i < files.length; i++) {
 					archivos[i]= new ReadFile();
 					int tmp[]=archivos[i].lee(files[i].toString());
 					
 
-					long inicioTemp = System.nanoTime();
+					//Usámos el método para medir el tiempo de inicio y final para hallar el tiempo de ejecución
+					long inicioTemp = System.nanoTime();      
 					QuickSort.sort(tmp, 0, tmp.length-1);
 					long finTemp = System.nanoTime();
 					long tTemp=finTemp-inicioTemp;
 					
+					//Digitamos los números ordenados, la dirección y el tiempo de ejecución en el archivo txt 
 					valores.add("Array Ordenado de "+files[i].toString()+" : "+Arrays.toString(tmp)+" con un tiempo de: "+String.valueOf(tTemp)+"nanosegundos");
 								
+					
 				     System.out.println(valores.get(i));
 		
 			}
 			
-			String pathResultado= "resultado.txt";
+			String pathResultado= "resultado.txt";   //nombramos el archivo final "resultado.txt"
 			CreateFile.creaDoc(valores,pathResultado);
 
-}
+    }
 }
